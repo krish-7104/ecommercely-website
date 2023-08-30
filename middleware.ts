676a9c6e2176aa903f9a259456140aab24a656c3
Krish Jotaniya <krishjotaniya71@gmail.com>
2023-08-30 13:40:17 +0530

    middleware fixes and env example added

--- middleware.ts
+++ middleware.ts
@@ -1,6 +1,5 @@
 import { NextResponse } from "next/server";
 import type { NextRequest } from "next/server";
-import jwt from "jsonwebtoken";
 
 export function middleware(request: NextRequest) {
   const path = request.nextUrl.pathname;
@@ -9,17 +8,12 @@ export function middleware(request: NextRequest) {
 
   const token = request.cookies.get("token")?.value || "";
 
-  if (!isPublicPath && !token) {
-    return NextResponse.redirect(new URL("/login", request.nextUrl));
+  if (isPublicPath && token) {
+    return NextResponse.redirect(new URL("/", request.nextUrl));
   }
 
-  if (token) {
-    try {
-      const decodedToken = jwt.verify(token, "your-secret-key");
-    } catch (error) {
-      console.error("Token verification failed:", error);
-      return NextResponse.redirect(new URL("/login", request.nextUrl));
-    }
+  if (!isPublicPath && !token) {
+    return NextResponse.redirect(new URL("/login", request.nextUrl));
   }
 }
 
