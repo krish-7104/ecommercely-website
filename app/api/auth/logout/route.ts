5d71e7e61f7bbf53dcc7d12db25eb6b4667547b9
Krish Jotaniya <krishjotaniya71@gmail.com>
2023-08-31 14:28:04 +0530

    logout api changed

--- app/api/auth/logout/route.ts
+++ app/api/auth/logout/route.ts
@@ -1,17 +1,15 @@
 import { NextResponse } from "next/server";
 
-export async function GET(req: Request) {
+export async function POST(req: Request) {
   try {
-    const response = NextResponse.json({
-      message: "Logout successful",
-      success: true,
-    });
-    response.cookies.set("token", "", {
-      httpOnly: true,
-      expires: new Date(0),
-    });
+    const response = new NextResponse("Logout successful");
+    response.headers.set(
+      "Set-Cookie",
+      `token=; HttpOnly; Path=/; Expires=${new Date(0).toUTCString()}`
+    );
     return response;
-  } catch (error: any) {
-    return NextResponse.json({ error: error.message }, { status: 500 });
+  } catch (error) {
+    console.log(error);
+    return new NextResponse("Internal Error", { status: 500 });
   }
 }
