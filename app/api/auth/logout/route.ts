e8a08e311eb470f90264d6fee152c961ce6ad872
Krish Jotaniya <krishjotaniya71@gmail.com>
2023-09-04 19:11:36 +0530

    Add expiry date before one year

--- app/api/auth/logout/route.ts
+++ app/api/auth/logout/route.ts
@@ -3,9 +3,11 @@ import { NextResponse } from "next/server";
 export async function GET(req: Request) {
   try {
     const response = new NextResponse("Logout successful");
+    const expirationDate = new Date();
+    expirationDate.setFullYear(expirationDate.getFullYear() - 1);
     response.headers.set(
       "Set-Cookie",
-      `token=; HttpOnly; Path=/; Expires=${new Date(0).toISOString()}`
+      `token=; HttpOnly; Path=/; Expires=${expirationDate.toUTCString()}`
     );
     return response;
   } catch (error) {
