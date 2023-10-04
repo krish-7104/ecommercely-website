890ff87f8bb9cbfc08cd8cc756f40df872f3a8be
Krish Jotaniya <krishjotaniya71@gmail.com>
2023-10-04 23:03:02 +0530

    Footer removed

--- app/layout.tsx
+++ app/layout.tsx
@@ -24,7 +24,7 @@ export default function RootLayout({
           <Navbar />
           {children}
           <Toaster position="bottom-right" />
-          <Footer />
+          {/* <Footer /> */}
         </Providers>
       </body>
     </html>
