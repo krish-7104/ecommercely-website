9d61ae475d8d7530e979e82aca10678c853c97e6
Krish Jotaniya <krishjotaniya71@gmail.com>
2024-04-06 17:03:29 +0530

    Cloudinary image issue solve

--- next.config.js
+++ next.config.js
@@ -1,7 +1,7 @@
 /** @type {import('next').NextConfig} */
 const nextConfig = {
     images: {
-        domains: ['m.media-amazon.com'],
+        domains: ['m.media-amazon.com',"res.cloudinary.com"],
     },
 }
 
